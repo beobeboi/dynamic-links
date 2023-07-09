a79bf8d2b00208c42662b5e3fd64daadf89acbef
Bradley Nguyen <giang.nguyen@diag.vn>
2023-07-09 12:52:31 +0700

    Build re-try

--- lib/Client.php
+++ lib/Client.php
@@ -16,13 +16,13 @@ class Client
 
     public function createLink(string $link, ?string $title = null, ?string $description = null, ?string $image = null, string $suffix = 'SHORT')
     {
-        if(empty($title)) {
+        if (empty($title)) {
             $title = 'Trang chủ - Diag';
         }
-        if(empty($description)) {
+        if (empty($description)) {
             $description = 'Từ năm 1998 đến nay, Diag luôn nỗ lực nâng cao hiệu quả khám chữa bệnh tại Việt Nam qua dịch vụ xét nghiệm tối ưu hóa, đáng tin cậy.';
         }
-        if(empty($image)) {
+        if (empty($image)) {
             $image = 'https://diag.vn/wp-content/uploads/2023/05/img2.3ce88933.png';
         }
         $option['json'] = [
@@ -32,7 +32,7 @@ class Client
                 'socialMetaTagInfo' => [
                     'socialTitle' => $title,
                     'socialDescription' => $description,
-                    'socialImageLink' => $image
+                    'socialImageLink' => $image,
                 ],
             ],
             'suffix' => [
@@ -42,12 +42,31 @@ class Client
         $option['query'] = [
             'key' => config('dynamic_link.api_key'),
         ];
-        $response = $this->client->request(
-            'POST',
-            $this->domain . '/' . $this->version . '/shortLinks',
-            $option
-        );
+        $running = true;
+        $tried = 1;
+        $response = null;
+        while ($running) {
+            try {
+                $response = $this->client->request(
+                    'POST',
+                    $this->domain . '/' . $this->version . '/shortLinks',
+                    $option
+                );
+                $running = false;
+            } catch (\Exception $exception) {
+                report($exception);
+                if ($tried > 3) {
+                    $running = false;
+                } else {
+                    $tried++;
+                }
+            }
+        }
 
-        return json_decode($response->getBody()->getContents(), true);
+        if ($response) {
+            return json_decode($response->getBody()->getContents(), true);
+        } else {
+            return [];
+        }
     }
 }
